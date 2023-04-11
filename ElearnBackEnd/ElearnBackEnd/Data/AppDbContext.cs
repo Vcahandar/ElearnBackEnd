87e29dba427ff7594a7cedf50651ac9834edc392
unknown <jahandarmv@code.edu.az>
2023-04-12 03:40:28 +0400

    new task

---
+++ ElearnBackEnd/ElearnBackEnd/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using ElearnBackEnd.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElearnBackEnd.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<CourseImage> CourseImages { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<News> News { get; set; }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ ElearnBackEnd/ElearnBackEnd/Program.cs
@@ -0,0 +1,43 @@
+using ElearnBackEnd.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapControllerRoute(
+//      name: "areas",
+//      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+//    );
+//});
+
+app.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
